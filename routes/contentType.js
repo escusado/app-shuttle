8828208b3586b17a4d795edebf1837220be97f0c
joaquin benitez <joaquin.benitez@gmail.com>
2012-10-07 01:37:13 -0500

    Custom homepage implementation

--- routes/contentType.js
+++ routes/contentType.js
@@ -1,4 +1,8 @@
 //content-type
 exports.index = function(req, res){
-  res.render('layout', { title: 'Content-type', view: 'content-type/index' });
+  res.render('layout', {
+    title   : 'App Shuttle - Content-types',
+    assets  : 'content-type/assets',
+    body    : 'content-type/body'
+  });
 };
