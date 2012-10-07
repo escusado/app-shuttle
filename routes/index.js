c5e3ab42ec44c1f66fb9fc40b3b89edfe99e4fd7
joaquin benitez <joaquin.benitez@gmail.com>
2012-10-06 23:46:38 -0500

    renamed views

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
 //Homepages
 exports.index = function(req, res){
-  res.render('layout', { title: 'App Shuttle', view: 'main' });
+  res.render('layout', { title: 'App Shuttle', assets: 'content-type/assets', body: 'content-type/body' });
 };
